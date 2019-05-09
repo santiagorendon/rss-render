4e97cc3c37fbd954d666bf62d91ea6cd3b671daa
Santiago Rendon <santiagorendon@Santiagos-MacBook-Air.local>
2019-05-09 06:11:44 -0400

    added cacheing

--- server.js
+++ server.js
@@ -3,15 +3,39 @@ const app = express();
 let Parser = require('rss-parser');
 let parser = new Parser();
 const path = require('path');
+const mcache = require('memory-cache');
 
 app.use(express.urlencoded({extended: false}));
 
-app.post('/api/rss', (req, res)=>{
+const cache = (duration) => {
+  return (req, res, next) => {
+    console.log(req.body.url);
+    let key = '__express__' + req.body.url;
+    console.log(key);
+    let cachedBody = mcache.get(key);
+    if(cachedBody){
+      console.log(`cached for ${duration} seconds`);
+      res.send(cachedBody);
+      return;
+    } else{
+      res.sendResponse = res.send;
+      res.send = (body) => {
+        mcache.put(key, body, duration * 1000);
+        res.sendResponse(body);
+      }
+      next();
+    }
+  }
+}
+
+//each page stays cached for 20 seconds
+//so refreshing the page will not fetch rss api every time
+//and therefore less likely chance of overusing api and reaching rate limits
+app.post('/api/rss', cache(20), (req, res)=>{
   (async () => {
   const url = req.body.url;
-
   let feed = await parser.parseURL(url);
-
+  console.log('not cached');
   const rss = feed.items;
   res.json([feed.title, rss]);
 })();
